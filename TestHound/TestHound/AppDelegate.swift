5be667c841e3a0592f734de2344449b6b5c215cf
Mikhail Zhigulin <mzhigulin@gmail.com>
2022-06-14 11:52:18 +0700

    Set up the project

---
+++ TestHound/TestHound/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestHound
+//
+//  Created by Mikhail Zhigulin on 14.06.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
